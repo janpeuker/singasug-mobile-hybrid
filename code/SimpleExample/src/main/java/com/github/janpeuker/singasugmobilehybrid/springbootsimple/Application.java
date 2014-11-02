147318d3fff8f226d1a4afe85b2ac8b020a794f5
Jan Peuker <devel@janpeuker.de>
2014-11-02 21:42:53 +0800

    Jan: Optimize imports

--- code/SimpleExample/src/main/java/com/github/janpeuker/singasugmobilehybrid/springbootsimple/Application.java
+++ code/SimpleExample/src/main/java/com/github/janpeuker/singasugmobilehybrid/springbootsimple/Application.java
@@ -3,7 +3,6 @@ package com.github.janpeuker.singasugmobilehybrid.springbootsimple;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 /**
  * Created by janpeuker on 22/10/14.
--- code/SimpleExample/src/test/java/com/github/janpeuker/singasugmobilehybrid/springbootsimple/service/StockPriceServiceTest.java
+++ code/SimpleExample/src/test/java/com/github/janpeuker/singasugmobilehybrid/springbootsimple/service/StockPriceServiceTest.java
@@ -1,6 +1,7 @@
 package com.github.janpeuker.singasugmobilehybrid.springbootsimple.service;
 
 import com.github.janpeuker.singasugmobilehybrid.springbootsimple.Application;
+import com.github.janpeuker.singasugmobilehybrid.springbootsimple.service.StockPriceService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
